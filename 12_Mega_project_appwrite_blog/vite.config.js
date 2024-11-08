58cf5425d1ca599f62598a86dc5c401786e1b7ee
davilgaming084 <davilgaming084@gmail.com>
2024-11-09 01:51:00 +0530

    update

--- 12_Mega_project_appwrite_blog/vite.config.js
+++ 12_Mega_project_appwrite_blog/vite.config.js
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
-      }
-    }
-  }
-}) 
+})
